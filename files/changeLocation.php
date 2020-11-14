89437637f08c61fa83d1db9263fa63a64a9b88b0
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-14 21:54:06 +0000

    Add changeLocation logic. Possible locations: kitchen, woods, bathroom.

--- files/changeLocation.php
+++ files/changeLocation.php
@@ -1,9 +1,30 @@
 <?php
   // Change player location
 function changeLocation(){	
-  // Write your code here:
+  global $location; 
   
-  
-  
-  
+  //determine where the player wants to go
+  echo "Where do you want to go?\n";
+  $response = readline(">> "); 
+  $response = strtolower($response);
+  echo "\n"; 
+
+  //logic for moving around
+  if($location === "kitchen" && $response === "bathroom"){
+    echo "You go to bathroom."; 
+    $location = $response; 
+  }elseif($location === "kitchen" && $response === "woods"){
+    echo "You follow the winding path, shivering as you make your way deep into the Terror Woods."; 
+    $location = $response;
+  }elseif($location ==="bathroom" && $response === "kitchen"){
+    echo "You go to kitchen."; 
+    $location = $response;
+  }elseif($location ==="woods" && $response === "kitchen"){
+    echo "You go to kitchen."; 
+    $location = $response;
+  }elseif($response === "bathroom" || $response === "kitchen" || $response === "woods"){
+    echo "You can't go directly to there from your current location. Try going somewhere else first.\n"; 
+  }else{
+    echo "That doesn't make sense. Are you confused? Try 'look around'.\n"; 
+  }
 }
