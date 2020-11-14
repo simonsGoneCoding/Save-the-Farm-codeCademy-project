2475580563832be48599ba6e972b6bd7956adc56
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-14 22:31:21 +0000

    Add logic for pickMushrooms and cookSoup.

--- files/cookSoup.php
+++ files/cookSoup.php
@@ -1,9 +1,13 @@
 <?php
   // Change player location
 function cookSoup(){	
-  // Write your code here:
-  
-  
-  
-  
+  global $location, $has_mushrooms, $has_soup;
+  //only in kitchen and with mushrooms; 
+  if(!($location === "kitchen" && $has_mushrooms)){
+    echo "You can't cook like this! You need something to cook AND to be in the kitchen.\n"; 
+  }else{
+    echo "You made some mushroom soup. Mushroom is the queen of all soups!\n"; 
+    $has_soup = !$has_soup;
+    $has_mushrooms = !$has_mushrooms;
+  }
 }
--- files/pickMushrooms.php
+++ files/pickMushrooms.php
@@ -1,11 +1,12 @@
 <?php
   
 function pickMushrooms(){
-	// Write your code here:
-  
-  
-  
-  
-  
-  
+	global $location, $has_mushrooms; 
+	//only available in woods
+	if($location !== "woods"){
+		echo "There aren't any mushrooms to pick here!\n"; 
+	}else{
+		echo "You've picked some mushrooms.\n"; 
+		$has_mushrooms === TRUE; 
+	}
 }  
