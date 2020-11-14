28934723ceb30f9388b6a33c37f96f5e9d5636b1
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-14 21:14:34 +0000

    Update printStatus file, add conditionals checking global variables and printing info for the player.

--- files/printStatus.php
+++ files/printStatus.php
@@ -5,7 +5,30 @@ function printStatus(){
   
     echo "You are in: $location\n";
   
-  // Write your code here:
+  if($wearing_glasses){
+    echo "You are wearing glases.\n"; 
+  }
+
+  if($wearing_contacts){
+    echo "You are wearing contacts.\n";
+  }
   
+  if($has_mushrooms){
+    echo "You are holding mushrooms.\n"; 
+  }
+
+  if($has_soup){
+    echo "You are holding a scalding-hot bowl of mushroom soup.\n"; 
+  }
+
+  if($needs_to_pee){
+    echo "You need to pee!\n"; 
+  }
+
+  if($is_hungry){
+    echo "You are hungry.\n"; 
+  }else{
+    echo "You are well-fed and energetic.\n"; 
+  }
 
 }
