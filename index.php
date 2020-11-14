c17573dff9200b8324ee13d39d7189264a5a3632
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-14 23:46:23 +0000

    Delete unwanted echo

--- index.php
+++ index.php
@@ -35,7 +35,6 @@ echo "\nOk, the game is about to begin. Will you be able to find the golden mous
 
 echo "\nHello there. It's been a harrowing few weeks. First your toenail issue, and now Great-Aunt Natasha's emu farm is in danger of being reposessed! So here you are---after a brief stopover at the Mayo clinic---in uncle Boris's remote cabin in the heart of the Terror Woods. Family legend holds that a golden statue of immense value is hidden somewhere within these walls. Unfortunately, you have a lot of stuff to do today, so you can only devote about 25 minutes to finding the statue. I guess we'll see what happens ;) \n\n";
 
-echo "grzyby : $has_mushroom";
 // Play 25 rounds
 gameRound();
 gameRound();
